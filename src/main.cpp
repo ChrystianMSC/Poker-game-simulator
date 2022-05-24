156e2f9705d15d9384bbeff40a37de60c48f31e2
Chrystian Martins <chrystianmsc@gmail.com>
2022-05-24 14:23:11 -0300

    Hand reset and semi table turns

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main() {
 
-    int n, di, j, pin, pote, bet;
+    int n = 0, di = 0, j = 0, pin = 0, pote = 0, bet = 0;
     string name;
 
     Player table[8];
@@ -29,8 +29,10 @@ int main() {
     inFile >> di;
     for (int i = 0; i < n; i++)
     {
+        pote = 0;
         if (i == 0)
         {
+            /* Set players and hands */
             inFile >> j;
             if (j < 2 && j >8)
             {
@@ -53,17 +55,44 @@ int main() {
                 char suit;
                 for (int z = 0; z < 5; z++)
                 {
-                    cin >> num;
-                    cin >> suit;
+                    inFile >> num;
+                    inFile >> suit;
                     card.Setcard(num, suit);
                     p.Setcardhand(card);
                 }
-                table[j] = p;
+                table[x] = p;
             }
+            /* Set players and hands - END */
+
+            /* See who wins */
+            for (int x = 0; x < j; x++)
+            {
+                cout << table[x].Getname() << " " << table[x].Getmoney() << endl; 
+            }
+            cout << endl;
+
+            /* See who wins - END */
+
+            /* Reset the hands */
+            for (int x = 0; x < j; x++)
+            {
+                Card card;
+                for (int z = 0; z < 5; z++)
+                {
+                    table[x].Setcardhand(card);
+                }
+            }
+            /* Reset the hands - END */
 
         }
         else
         {
+            /* Set players hands */
+            Player semitable[8];
+            for (int f = 0; f < 8; f++)
+            {
+                semitable[f] = nullplayer;
+            }
             int aux = j;
             inFile >> j;
             if (j < 2 && j >8)
@@ -73,12 +102,60 @@ int main() {
             }
             inFile >> pin;
 
-            for (int i = 0; i < aux; i++)
+            for (int k = 0; k < j; k++)
             {
                 inFile >> name;
+                for (int x = 0; x < aux; x++)
+                {
+                    if (table[x].Getname() == name)
+                    {
+                        inFile >> bet;
+                        table[x].Setmoney(table[x].Getmoney() - bet);
+                        pote += bet;
+
+                        Card card;
+                        int num;
+                        char suit;
+                        for (int z = 0; z < 5; z++)
+                        {
+                            inFile >> num;
+                            inFile >> suit;
+                            card.Setcard(num, suit);
+                            table[x].Setcardhand(card);
+                        }
+                        semitable[k] = table[x];
+                    }
+                    else
+                    {
+                        table[x].Setmoney(table[x].Getmoney() - pin);
+                        pote += pin;
+                    }
+                    
+                    
+                }
+                
             }
-            
-            
+            /* Set players hands - END */
+
+            /* See who wins semitable */
+            for (int x = 0; x < j; x++)
+            {
+                cout << semitable[x].Getname() << " " << semitable[x].Getmoney() << endl; 
+            }
+            cout << endl;
+
+            /* See who wins semitable - END */
+
+            /* Reset the hands */
+            for (int x = 0; x < j; x++)
+            {
+                Card card;
+                for (int z = 0; z < 5; z++)
+                {
+                    table[x].Setcardhand(card);
+                }
+            }
+            /* Reset the hands - END */
         }
 
     }
